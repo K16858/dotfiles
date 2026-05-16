657e3889d6984f4fcb2b744c1906806a948ca247
k16858 <ktohda0127@gmail.com>
2026-05-16 19:03:34 +0900

    fix: use copy instead of symlink for hypr

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 echo "Setting up dotfiles..."
 
-ln -sf ~/.dotfiles/hypr ~/.config/hypr
-ln -sf ~/.dotfiles/kitty ~/.config/kitty
-ln -sf ~/.dotfiles/waybar ~/.config/waybar
-ln -sf ~/.dotfiles/rofi ~/.config/rofi
+mkdir -p ~/.config/hypr
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/rofi
+
+cp ~/.dotfiles/hypr/* ~/.config/hypr/
+cp ~/.dotfiles/kitty/* ~/.config/kitty/
+cp ~/.dotfiles/waybar/* ~/.config/waybar/
+cp ~/.dotfiles/rofi/* ~/.config/rofi/
 
 echo "Done!"
