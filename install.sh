9342dcf29052f38020f89459e2258a3f5d1ead9d
k16858 <ktohda0127@gmail.com>
2026-05-17 20:53:24 +0900

    add: GRUB and SDDM themes

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "Setting up dotfiles..."
 
+# Config files
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/kitty
 mkdir -p ~/.config/waybar
@@ -11,5 +12,12 @@ cp ~/.dotfiles/hypr/* ~/.config/hypr/
 cp ~/.dotfiles/kitty/* ~/.config/kitty/
 cp ~/.dotfiles/waybar/* ~/.config/waybar/
 cp ~/.dotfiles/rofi/* ~/.config/rofi/
+
+# GRUB theme
+sudo cp -r ~/.dotfiles/grub/catppuccin-frappe-grub-theme /usr/share/grub/themes/
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# SDDM theme
+sudo cp -r ~/.dotfiles/sddm/themes/catppuccin-frappe-mauve /usr/share/sddm/themes/
 
 echo "Done!"
